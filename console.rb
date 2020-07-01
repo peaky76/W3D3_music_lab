2daa79272e8f85faa9ce05107ca60e9e1e76fea0
peaky76 <robertjamespeacock@gmail.com>
2020-07-01 14:34:28 +0100

    console setup

---
+++ console.rb
@@ -0,0 +1,6 @@
+require('pry')
+require_relative('models/album')
+require_relative('models/artist')
+
+binding.pry
+nil
--- models/artist.rb
+++ models/artist.rb
@@ -10,7 +10,12 @@ class Artist
         @name = options['name']    
     end
 
-    # def save()
-    #     sql = "INSERT into "
+    def save()
+        sql = "INSERT INTO artists
+        (name) VALUES ($1) RETURNING id"
+        values = [@name]
+        result = SqlRunner.new(sql, values)
+        @id = result[0]['id'].to_i()
+    end
 
 end
